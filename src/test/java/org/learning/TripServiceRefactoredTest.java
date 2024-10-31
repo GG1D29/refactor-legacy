7f9ec27548e55dce737299426e8f957c7015a01e
GG <GG1D29@users.noreply.github.com>
2024-10-31 10:49:59 +0700

    Test 1: Introduce constant for better storytelling

--- src/test/java/org/learning/TripServiceRefactoredTest.java
+++ src/test/java/org/learning/TripServiceRefactoredTest.java
@@ -5,17 +5,23 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class TripServiceRefactoredTest {
+    private static final User GUEST = null;
+    private static final User ANY_USER = new User();
+    private User loggedInUser;
+
     @Test
     void should_ThrowsException_When_UserNotLoggedIn() {
         TripService tripService = new TestableTripService();
+        loggedInUser = GUEST;
 
-        assertThrows(UserNotLoggedInException.class, () -> tripService.getTripsByUser(null));
+        assertThrows(UserNotLoggedInException.class,
+                () -> tripService.getTripsByUser(ANY_USER));
     }
 
     private class TestableTripService extends TripService {
         @Override
         User getLoggedInUser() {
-            return null;
+            return loggedInUser;
         }
     }
 }
